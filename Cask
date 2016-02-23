ed6f0f67f3dc6eaec4cb50bff293c3016da53f60
Stig Brautaset <stig@brautaset.org>
2016-02-23 23:39:35 +0000

    Initial commit - starting to get a skeleton together

---
+++ Cask
@@ -0,0 +1,10 @@
+(source gnu)
+(source melpa)
+
+(package-file "ox-jira.el")
+
+(development
+ (depends-on "f")
+ (depends-on "ecukes")
+ (depends-on "ert-runner")
+ (depends-on "el-mock"))
---
+++ ox-jira.el
@@ -0,0 +1,16 @@
+
+;;; ox-jira.el --- an Org mode export backend for JIRA format
+
+;;; Copyright (C) 2016 Stig Brautaset
+
+;;; Author: Stig Brautaset <stig@brautaset.org>
+
+;;; Version: 0.1-SNAPSHOT
+
+;; Homepage: https://github.com/stig/ox-jira.el
+
+;; Package-Requires: ((org "8.0"))
+
+;;; Code:
+
+;;; ox-jira.el ends here
