9d7a4bd002efd57e80e96fc842ab133e466461ab
Stig Brautaset <stig@brautaset.org>
2016-02-29 09:41:11 +0000

    Use let rather than setq in build helper script

--- tangle-org-files.el
+++ tangle-org-files.el
@@ -1,9 +1,13 @@
 (require 'org)
 
 ;; Find our current directory.
-(setq dir (file-name-directory (or (buffer-file-name) load-file-name)))
+(let* ((dir (file-name-directory (or (buffer-file-name) load-file-name)))
+       (test-dir (expand-file-name "test" dir)))
 
-;; Generate *.el files from *.org files in `.' and the `test/' directories.
-;; No doubt this could be done in one statement, but my elisp-fu is not very strong yet.
-(mapc #'org-babel-load-file (directory-files dir t "ox-.*\\.org$"))
-(mapc #'org-babel-load-file (directory-files (expand-file-name "test" dir) t "ox-.*\\.org$"))
+  ;; Tangle source files
+  (mapc #'org-babel-load-file
+        (directory-files dir t "ox-.*\\.org$"))
+
+  ;; Tangle test files
+  (mapc #'org-babel-load-file
+        (directory-files test-dir t "ox-.*\\.org$")))
